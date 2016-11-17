e5b710db9dd41d30c5e9319ae013e8472dfa7812
VikramGuleria <VikramGuleria@users.noreply.github.com>
2016-11-18 02:04:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
+app.get('/article-one',function(req,res) {
+   res.send('article one is requested and willl be served here');
+    });
+    
 app.get('/article-two',function(req,res) {
    res.send('article two is requested and willl be served here');
     });
