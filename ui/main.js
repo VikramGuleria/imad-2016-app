40c4e226c07c3320f99e18e34dd40dfccfba4c08
VikramGuleria <VikramGuleria@users.noreply.github.com>
2016-11-23 18:46:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 
-var madi =  document.getElementById('madi');
+var img =  document.getElementById('madi');
 img.onclick = function() {
     img.style.marginleft = '100px';
 };
