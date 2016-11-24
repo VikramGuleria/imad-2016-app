17a6529381c3d88bd2208a2a7a7101e0444c59df
VikramGuleria <VikramGuleria@users.noreply.github.com>
2016-11-24 23:42:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,20 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML = 'New Value';
- 
-var img =  document.getElementById('madi');
-var marginLeft = 0;
-  function moveRight () {
-     marginLeft = marginLeft + 1;
-     img.style.marginLeft = marginLeft + 'px';
-    }
-   img.onclick = function() {
-    var interval = setInterval(moveRight,10);
+var button = document.getElementById('counter');
+button.onclick = function(){
     
+ var request = new XMLHttpRequest();
+ 
+ request.onreadystatechange = function(){
+     if (request.readyState === XMLHttpRequest.DONE){
+     if (request.status === 200){    
+     var counter = request.responseText;
+      var span = document.getElementById('count');
+      span.innerHTML = counter.toString();   
+     }
+     }
+ };
+
+request.open('GET','http://vikramguleria.imad.hasura-app.io/counter',true);
+request.send(null);
+
+
 };
