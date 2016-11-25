96cc1534c49c9b868ce396519fad623ee6b059f5
VikramGuleria <VikramGuleria@users.noreply.github.com>
2016-11-25 19:02:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,11 +113,11 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var names=[];
+var names = [];
 
-app.get('/submit-name/:name',function(req,res) {
+app.get('/submit-name/:name',function(req, res) {
 var name = req.params.name;
-name.push(name);
+names.push(name);
 
 res.send(JSON.stringify(name));
     
