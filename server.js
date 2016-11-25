5e5f7ad04b6464d822e155c2611ffdfa9886b251
VikramGuleria <VikramGuleria@users.noreply.github.com>
2016-11-25 19:08:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,6 +92,15 @@ app.get('/counter',function(req,res){
 res.send(counter.toString());
 });
 
+var names = [];
+app.get('/submit-name/:name',function(req, res) {
+var name = req.query.name;
+names.push(name);
+
+res.send(JSON.stringify(names));
+    
+});
+
 app.get('/:articleName',function(req,res) {
     //articleName == article-one
     //articles[articleName]=={} content object for article one
@@ -113,15 +122,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var names = [];
 
-app.get('/submit-name/:name',function(req, res) {
-var name = req.params.name;
-names.push(name);
-
-res.send(JSON.stringify(name));
-    
-});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
