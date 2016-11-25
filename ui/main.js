5563e63babe54426a021895bd3f636a4a77fcce9
VikramGuleria <VikramGuleria@users.noreply.github.com>
2016-11-25 09:22:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,3 +18,19 @@ button.onclick = function() {
 request.open('GET','http://vikramguleria.imad.hasura-app.io/counter',true);
 request.send(null);
 };
+
+var nameInput = document.getElementById('name');
+var name= nameInput.value;
+var submit= document.getElementById('submit_btn');
+submit.onclick = function(){
+   
+   var names=['Name1','Name2','Name3'] ;
+   var list = '';
+   for (var i=0; i<names.length; i++){
+       list +- '<li>' +names[i] + '<li>';
+       
+   }
+   
+   var ul = document.getElementById('namelist');
+   ul.innerHTML = list;
+};
