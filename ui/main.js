29eb1d3454a93beb2536468e40aed9fe2802f2af
VikramGuleria <VikramGuleria@users.noreply.github.com>
2016-11-26 00:14:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 
 
-var img = document.getElementById('madi');
+var img = document.getElementById('madi1');
 var marginLeft = 0;
 function moveRight () {
     
@@ -10,7 +10,7 @@ function moveRight () {
 img.onclick = function(){
      var interval = setInterval(moveRight,-100);
 };
-var img = document.getElementById('madi');
+var img = document.getElementById('madi2');
 var marginRight = 0;
 function moveLeft () {
     
